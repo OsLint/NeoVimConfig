4a361b6df8417cbfab1be98cca5768828a6a741e
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2024-09-19 23:58:52 +0200

    lsp

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,4 +8,18 @@ return {
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
   },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function ()
+    require("telescope").setup({
+  extensions = {
+    ["ui-select"] = {
+      require("telescope.themes").get_dropdown {
+    }
+    }
+  }
+})
+  require("telescope").load_extension("ui-select")
+    end
+  },
 }
